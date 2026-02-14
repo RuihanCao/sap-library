344655d4e26ca386b45f5cad8662cc15baacc50a
Porter Young <porteryoung2005@gmail.com>
2026-02-14 16:33:51 -0700

    vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
-﻿import "./globals.css";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Sap Library",
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>
         {children}
+        <Analytics />
       </body>
     </html>
   );
